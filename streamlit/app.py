277e18e296b1b04d8324632634005f434502cf1d
Adham Soliman <adham.soliman@iis.fraunhofer.de>
2026-08-18 11:58:24 +0200

    Test remote deployment

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.set_page_config(page_title="Webapplication Farm Demo")
 
-st.title("Hello World")
+st.title("Hello World - version 2")
 st.write("Streamlit application deployed from Git.")
 st.write("Change this text, run Git Pull, and restart the app to test remote deployment.")
