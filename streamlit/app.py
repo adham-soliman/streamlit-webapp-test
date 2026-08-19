43ed55a39adfc3ba461abff79700f45e6e821453
adham-soliman <adham.soliman@iis.fraunhofer.de>
2026-08-19 14:09:46 +0200

    Update version title in Streamlit app

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.set_page_config(page_title="Webapplication Farm Demo")
 
-st.title("CI/CD TEST - Version 3333")
+st.title("CI/CD TEST - Version 666666")
 st.write("Streamlit application deployed from Git.")
 st.write("Change this text, run Git Pull, and restart the app to test remote deployment.")
